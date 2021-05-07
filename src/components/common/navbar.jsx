07151f380abd5b0e40c43812a9899934e07b5188
jordan819 <patryk.zaucha00@gmail.com>
2021-05-07 11:52:10 +0200

    add login form

--- src/components/common/navbar.jsx
+++ src/components/common/navbar.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
     return (
         <div>
             <ul className="nav nav-pills mb-3" id="pills-tab" role="tablist">
                 <li className="nav-item" role="presentation">
-                    <Link to="/" className="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
-                          aria-controls="pills-home" aria-selected="true">Home</Link>
+                    <NavLink to="/" exact className="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
+                          aria-controls="pills-home" aria-selected="true">Home</NavLink>
                 </li>
                 <li className="nav-item" role="presentation">
-                    <Link to="/posts" className="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
-                          aria-controls="pills-home" aria-selected="true">Posts</Link>
+                    <NavLink to="/posts" className="nav-link" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab"
+                          aria-controls="pills-home" aria-selected="true">Posts</NavLink>
                 </li>
+                <li className="nav-item" role="presentation">
+                   <NavLink to="/login" className="nav-link" id="pills-home-tab" data-toggle="pill" role="tab"
+                         aria-controls="pills-home" aria-selected="true">Login</NavLink>
+               </li>
             </ul>
         </div>
     );
